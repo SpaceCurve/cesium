2a614c4a816a50a1243c6d092c74b13e721ebd96
Brett L Kleinschmidt <brett@spacecurve.com>
2013-09-16 14:06:04 -0700

    Example of Cesium using socket.io

--- Apps/server.js
+++ Apps/server.js
@@ -1,5 +1,20 @@
 /*global require,__dirname*/
-var connect = require('connect');
-connect.createServer(
- connect.static(__dirname)
-).listen(8080);
+var con = require('connect');
+var app = con.createServer(con.static(__dirname)).listen(8000);
+var io = require('socket.io').listen(app, { log: false });
+io.sockets.on('connection', function (socket) {
+  socket.emit('connect', { hello: 'world' });
+  socket.on('query', function (data) {
+    console.log(data);
+    require('http').get(
+      'http://127.0.0.1:8080/db/' + encodeURIComponent(data.text),
+      function (response) {
+        if (response.statusCode == 200) {
+          response.on('data', function (chunk) { socket.emit('data', { chunk: chunk.toString() }); });
+          response.on('end', function () { socket.emit('end'); });
+        }
+        console.log('status:', response.statusCode);
+      }
+    );
+  });
+});
