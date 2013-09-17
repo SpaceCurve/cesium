7ac647babf2f42a756b83720519dd53da35c557d
Brett L Kleinschmidt <brett@spacecurve.com>
2013-09-17 13:10:26 -0700

    Always return HTTP status code back to client Make instance_name a UI parameter Make region selector use socket.io

--- Apps/server.js
+++ Apps/server.js
@@ -3,18 +3,21 @@ var con = require('connect');
 var app = con.createServer(con.static(__dirname)).listen(8000);
 var io = require('socket.io').listen(app, { log: false });
 io.sockets.on('connection', function (socket) {
-  socket.emit('connect', { hello: 'world' });
+  socket.emit('connect');
   socket.on('query', function (data) {
-    console.log(data);
+    console.log('Request:', data);
     require('http').get(
-      'http://127.0.0.1:8080/db/' + encodeURIComponent(data.text),
+      'http://127.0.0.1:8080/' + data.instance + '/' + encodeURIComponent(data.text),
       function (response) {
-        if (response.statusCode == 200) {
+        var data = { status: response.statusCode };
+        if (data.status == 200) {
           response.on('data', function (chunk) { socket.emit('data', { chunk: chunk.toString() }); });
-          response.on('end', function () { socket.emit('end'); });
+          response.on('end', function () { socket.emit('end', data); });
+        } else {
+          socket.emit('end', data); 
         }
-        console.log('status:', response.statusCode);
+        console.log('Response:', data);
       }
-    );
+    ).on('error', function (error) { console.log('Error:', error.message); });
   });
 });
